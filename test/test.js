0c96435c43c9df2893ed748c9dfe83657ebc8ffd
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-02-23 18:06:14 -0700

    object cleaner function working

--- test/test.js
+++ test/test.js
@@ -4,6 +4,9 @@ import { shallow, mount, render } from 'enzyme';
 import Main from '../lib/Components/Main';
 import Weatherly from '../lib/Components/Weatherly';
 import WeatherCards from '../lib/Components/WeatherCards';
+import ObjectCleaner from '../lib/Components/helper';
+
+require('locus');
 
 
 describe('testing Weatherly in enzyme', () => {
@@ -22,22 +25,81 @@ describe('testing Weatherly in enzyme', () => {
     const wrapper = shallow(<Weatherly/>);
     expect(wrapper.find('WeatherCards')).to.have.length(1);
   });
-  // url get function
-  // local storage
-  // state has location - input empty and weather object - empty
-  // add a location, stores location
-  // change location, reflected in change of state
-  // validation of locationin weatherly
+
+  it('Weatherly should update location state on input change', () => {
+    const wrapper = shallow(<Weatherly/>);
+    expect(wrapper.state().location).to.equal('');
+    wrapper.find('.location').simulate('change', {target: {value: 'denver'}})
+    expect(wrapper.state().location).to.equal('denver');
+  });
+
+  it('Should update location when a location is already set as the state.', () => {
+    const wrapper = shallow(<Weatherly/>);
+    wrapper.find('.location').simulate('change', {target: {value: 'denver'}})
+    expect(wrapper.state().location).to.equal('denver');
+    wrapper.find('.location').simulate('change', {target: {value: 'englewood'}})
+    expect(wrapper.state().location).to.equal('englewood');
+
+  });
+  it.skip('Weatherly should update weather state on button click', () => {
+    const wrapper = mount(<Weatherly/>);
+    expect(wrapper.state().weather).to.deep.equal([]);
+    wrapper.find('.location').simulate('change', {target: {value: 'denver, co'}});
+    wrapper.find('.submit').simulate('click', 1);
+    console.log(wrapper.state());
+    expect(wrapper.state().weather).to.equal([]);
+  });
+
+  it.skip('Test the URL func', () => {
+    // url get function
+
+  });
+
+  it.skip('Test the URL func', () => {
+    // local storage
+
+  });
+
+  it.skip('Test the URL func', () => {
+    // validation of locationin weatherly
+
+  });
 });
 
 describe('WeahterCards component testing in enzyme', () => {
   const weatherFake = require('./helpers/stub.json')
-  it.only('WeatherCards should have an Hourly component with 7 instantiations', () => {
+  it('WeatherCards should have an Hourly component with 7 instantiations', () => {
     const wrapper = shallow(<WeatherCards weather={ weatherFake } />);
 
     expect(wrapper.find('Hourly')).to.have.length(7);
   });
-  // hourly is a component - length
-  // ten day forecast - length
+
+  it('WeatherCards should have a TenDay component with 10 instantiations', () => {
+    const wrapper = shallow(<WeatherCards weather={ weatherFake } />);
+
+    expect(wrapper.find('TenDay')).to.have.length(10);
+  });
+
   // state is passed down to all components
 });
+describe('WeahterCards component testing in enzyme', () => {
+
+  const weatherFake = require('./helpers/stub.json')
+
+  it.only('WeatherCards should have an Hourly component with 7 instantiations', () => {
+    console.log(ObjectCleaner(weatherFake));
+    // expect(ObjectCleaner.hourlyForecast(weatherFake)).to.have.length(7);
+  });
+
+  it('WeatherCards should have an TenDay component with 10 instantiations', () => {
+
+    expect(ObjectCleaner.tenDayForecast(weatherFake)).to.have.length(10);
+  });
+
+  it.skip('WeatherCards should have an TenDay component with 10 instantiations', () => {
+
+    expect(ObjectCleaner.currentForecast(weatherFake)).to.have.length(10);
+  });
+});
+
+// test helper functions
